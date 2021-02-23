8898d376237b307ea6f90dc71422041de7227fbf
Daniel <d.lee.2@elsevier.com>
2021-02-23 21:11:21 +0000

    solution

--- src/components/PropertyListing/tests/PropertyListing.spec.js
+++ src/components/PropertyListing/tests/PropertyListing.spec.js
@@ -1,16 +1,45 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import {act} from 'react-dom/test-utils'
+import sinon from 'sinon'
+import {mount, shallow} from 'enzyme';
 import PropertyListing from '../PropertyListing';
 
 describe('PropertyListing', () => {
+    afterAll(() => {
+        sinon.restore();
+    })
 
     it('should render without crashing', () => {
         const wrapper = shallow(<PropertyListing/>);
         expect(wrapper.find('.PropertyListing')).toHaveLength(1);
     });
 
-    it('should render five property cards', () => {
-        const wrapper = shallow(<PropertyListing/>);
-        expect(wrapper.find('PropertyCard')).toHaveLength(5);
+    it('should render five property cards', async () => {
+        sinon.stub(global, 'fetch').resolves({
+            json: () => [...Array(5).keys()].map(()=>({
+                id: 73864112,
+                bedrooms: 3,
+                summary: 'Property 1 Situated moments from the River Thames in Old Chelsea...',
+                displayAddress: '1 CHEYNE WALK, CHELSEA, SW3',
+                propertyType: 'Flat',
+                price: 1950000,
+                branchName: 'M2 Property, London',
+                propertyUrl: '/property-for-sale/property-73864112.html',
+                contactUrl: '/property-for-sale/contactBranch.html?propertyId=73864112',
+                propertyTitle: '3 bedroom flat for sale',
+                mainImage: 'https://media.rightmove.co.uk/dir/crop/10:9-16:9/38k/37655/53588679/37655_CAM170036_IMG_01_0000_max_476x317.jpg'
+            }))
+        })
+
+        let wrapper
+        act(() => {
+            wrapper = mount(<PropertyListing/>)
+        });
+
+        // flush promises
+        await new Promise(setImmediate);
+        wrapper.update()
+
+        expect(wrapper.find('.PropertyCard')).toHaveLength(5);
     });
 });
